c5b5874d1c67e8d779bb431ab4999c3e9226ebff
Narek Bojikian <narekb95@gmail.com>
2025-02-21 20:10:15 +0100

    added remove comments and prilemenary expand

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 long: main.tex
-	python3 ignore-if.py --input main.tex --output long.tex --conditions "long:false,short:true"
+	python3 ignore-if.py --input main.tex --output long.tex --conditions "long:false,short:true" --recursive
 
 short: main.tex
 	python3 ignore-if.py --input main.tex --output short.tex --conditions "long:true,short:false"
--- ignore-if.py
+++ ignore-if.py
@@ -3,7 +3,7 @@
 # 5. delete newif and longrue longfalse
 # 7. add command line argument to delete comments
 
-
+from os import path
 import re
 import argparse
 
@@ -21,19 +21,63 @@ def handle_output(line, curr_pos, match, matched_condition):
 
     return out
 
+def find_comment_start(line: str) -> int:
+    """
+    Returns the index where the comment starts in a LaTeX line.
+    If no unescaped % is found, returns -1.
+    """
+    for i, ch in enumerate(line):
+        if ch == '%':
+            # Count consecutive backslashes immediately before the %
+            backslash_count = 0
+            j = i - 1
+            while j >= 0 and line[j] == '\\':
+                backslash_count += 1
+                j -= 1
+            # If backslash_count is even, then % is not escaped
+            if backslash_count % 2 == 0:
+                return i
+    return -1
 
 def reduce_comment(line):
-    # TODO: possible problem: '\\\\%' break line before comment
-    comment = re.match(r'(?<!\\)%.*', line)
-    if comment:
-        pos = comment.start()
-        comment = line[pos:]
-        line = line[:pos]
-    else:
-        comment = ''
-    return line, comment
+    start = find_comment_start(line)
+    if start == -1:
+        return line, ''
+    return line[:start], line[start:]
 
-def process_latex_file(input_filename, output_filename, conditions):
+def expand_input(input_filename, base_path):
+    out_path = path.join(base_path, 'tmp.tex')
+
+    input_pat = re.compile(r'\\input{(.+)}')
+    text = None
+    with open(input_filename, 'r', encoding='utf-8') as infile:
+        text = infile.read()
+
+    prefix = ''
+    last_pos = 0
+    for match in input_pat.finditer(text):
+        input_file = match.group(1)
+        if not input_file.endswith('.tex'):
+            input_file += '.tex'
+        input_file = path.join(base_path, input_file)
+        prefix += text[last_pos:match.start()]
+        with open(input_file, 'r', encoding='utf-8') as infile:
+            prefix += infile.read()
+        last_pos = match.end()
+    prefix += text[last_pos:]
+    with open(out_path, 'w', encoding='utf-8') as outfile:
+        outfile.write(prefix)
+    return out_path
+
+        
+
+# [TODO]: only expand if in output context, and recursively line by line directly to output (implement function process_line and call recursively on input)
+def process_latex_file(input_path, output_filename, conditions, delete_comments, recursive):
+    base_path = path.dirname(input_path)
+    out_path = path.join(base_path, output_filename)
+    if recursive:
+        input_filename = expand_input(input_path, base_path)
+    base_path = path.dirname(input_path)
     # Stack to keep track of \ifword conditions
     ignore_count = 0  # Counter to track nested \if{condition}
 
@@ -44,14 +88,18 @@ def process_latex_file(input_filename, output_filename, conditions):
 
 
 
-    with open(input_filename, 'r', encoding='utf-8') as infile, open(output_filename, 'w', encoding='utf-8') as outfile:
+    with open(input_filename, 'r', encoding='utf-8') as infile, open(out_path, 'w', encoding='utf-8') as outfile:
         stack = []
 
-        for line in infile:
+        for i, line in enumerate(infile):
+            # If line is empty and ignore_count is 0, write it
+            # Empty output will be ignored otherwise
             if ignore_count == 0 and len(line.strip()) == 0:
                 outfile.write(line)
                 continue
             line, comment = reduce_comment(line)
+            if delete_comments and comment.endswith('\n'):
+                comment = '\n'
 
             newif_match = newif_pattern.search(line)
             if newif_match:
@@ -61,7 +109,6 @@ def process_latex_file(input_filename, output_filename, conditions):
                 continue
             
             out=''
-            # get all matches of if{condition}, else, fi using matchall
             all_matches = re.finditer(r'\\if(\w+)|\\else|\\fi', line)
             curr_match = next(all_matches, None)
             start_pos = 0
@@ -71,9 +118,7 @@ def process_latex_file(input_filename, output_filename, conditions):
                     return [match.group(1), 'if']
                 elif match.group(0) in ['\\else', '\\fi']:
                     return [stack_top_condition, match.group(0)[1:]]
-                raise ValueError("Invalid match", match.group(0))
-                return None
-                
+                raise ValueError("Invalid match", match.group(0))              
 
             while curr_match:
                 curr_command = get_current_condition(curr_match, stack[-1][0] if len(stack) else None)
@@ -106,7 +151,6 @@ def process_latex_file(input_filename, output_filename, conditions):
                 curr_match = next(all_matches, None)
 
             if ignore_count == 0:
-                # print rest and comment
                 out += line[start_pos:] + comment
             # if lien is empty now (wasn't empty before) don't write it
             # else might start paragraph
@@ -118,14 +162,21 @@ def main():
     parser.add_argument('--input', required=True, help='Input LaTeX file')
     parser.add_argument('--output', required=True, help='Output LaTeX file')
     parser.add_argument('--conditions', required=True, help='Conditions to ignore in the format "condition1:true,condition2:false".\\Use true to remove the "if" branch, and false to remove the "else" branch.')
+    parser.set_defaults(delete_comments=False)
+    parser.add_argument('--delete-comments', action='store_true', help='Delete comments.')
+
+    parser.add_argument('--recursive', action='store_true', help='Recursively expand input commands.')
+    parser.set_defaults(recursive=False)
 
     args = parser.parse_args()
 
     input_filename = args.input
     output_filename = args.output
     conditions = [(cond.split(':')[0], cond.split(':')[1].lower() == 'true') for cond in args.conditions.split(',')]
+    delete_comments = args.delete_comments
+    recursive = args.recursive
 
-    process_latex_file(input_filename, output_filename, conditions)
+    process_latex_file(input_filename, output_filename, conditions, delete_comments, recursive)
 
 
 if __name__ == '__main__':
